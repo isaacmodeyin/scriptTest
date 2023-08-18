230e5cfdb2c2345e32156f37646051e96e2c6ba3
Modeyin Isaac Oluwadamilare <111261798+isaacmodeyin@users.noreply.github.com>
2023-08-18 17:19:27 +0100

    double
    to check for speed deployning

--- codeScript.js
+++ codeScript.js
@@ -1,3 +1,6 @@
 function myFunction() {
     document.getElementById("demo").innerHTML="Paragraph changed.";
 }
+function myFunction() {
+    document.getElementById("demo").innerHTML="Paragraph changed.";
+}
